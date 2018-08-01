f744a47823d08d0db3dadb240d09b0fdb02ccf2f
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-08-01 14:17:17 -0600

    CA-238: Add IconParagraph atom and Benefits component that uses it

---
+++ src/components/atoms/IconParagraph.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const IconParagraph = ({
+    icon,
+    icon_class,
+    icon_heading,
+    icon_text
+    }) => {
+
+    let iconClass = `iconparagraph__wrapper ${ icon_class }`;
+    let image;
+
+    if(icon) {
+        image = <div className = "iconparagraph__icon"><img src = { icon } alt = { icon_heading } /></div>
+    }
+
+    return (
+        <div className = { iconClass }>
+            { image }
+            <p className = "iconparagraph__heading"> { icon_heading } </p>
+            <p> { icon_text } </p>
+        </div>
+    )
+
+};
+
+export default IconParagraph;
+
+IconParagraph.propTypes = {
+    icon_class: PropTypes.string,
+    icon_heading: PropTypes.string,
+    icon_text: PropTypes.string,
+};
---
+++ src/components/organisms/Benefits.js
@@ -0,0 +1,18 @@
+import React from "react";
+import SectionTitle from "../atoms/SectionTitle";
+
+const Benefits = () => (
+    <section
+        className = "section section_benefits usa-grid">
+
+        <SectionTitle title = "Benefits"/>
+        <div
+            className = "">
+
+        </div>
+
+    </section>
+
+);
+
+export default Benefits;
