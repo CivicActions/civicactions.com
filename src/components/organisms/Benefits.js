351fe3c6033d071b42a7dc7fa791b236bcecf4ad
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-08-01 15:24:02 -0600

    CA-238: Place IconParagraph atom in Benefits component

--- src/components/organisms/Benefits.js
+++ src/components/organisms/Benefits.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SectionTitle from "../atoms/SectionTitle";
+import IconParagraph from "../atoms/IconParagraph";
 
 const Benefits = () => (
     <section
@@ -7,8 +8,43 @@ const Benefits = () => (
 
         <SectionTitle title = "Benefits"/>
         <div
-            className = "">
-
+            className = "iconparagraphs__group">
+            <IconParagraph
+                icon = ""
+                icon_heading = "Benefits"
+                icon_text = ""
+                icon_class = "icon__benefits"
+            />
+            <IconParagraph
+                icon = ""
+                icon_heading = "Time Off"
+                icon_text = ""
+                icon_class = "icon__timeoff"
+            />
+            <IconParagraph
+                icon = ""
+                icon_heading = "Family"
+                icon_text = ""
+                icon_class = "icon__family"
+            />
+            <IconParagraph
+                icon = ""
+                icon_heading = "Connection"
+                icon_text = ""
+                icon_class = "icon__connection"
+            />
+            <IconParagraph
+                icon = ""
+                icon_heading = "Growth"
+                icon_text = ""
+                icon_class = "icon__growth"
+            />
+            <IconParagraph
+                icon = ""
+                icon_heading = "Commmunity"
+                icon_text = ""
+                icon_class = ""
+            />
         </div>
 
     </section>
