431b00c39ad317cd73fc4d6b9428a921f9db1ec5
Iris Ibekwe <iris.ibekwe@civicactions.com>
2018-08-09 19:34:32 -0400

    CA-239: Add a subtitle to the Section Titles.

--- src/components/atoms/SectionTitle.js
+++ src/components/atoms/SectionTitle.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const SectionTitle = ({title, subtitle}) => (
-  <div className = "section__title--wrapper">
-    <h2 className = "section__title">{ title }</h2>
-    <p className = "section__subtitle">{ subtitle }</p>
-  </div>
-);
+const SectionTitle = ({title, subtitle}) => {
+  const sectionSubtitle = subtitle ? <p className = "section__subtitle">{ subtitle }</p>: '';
+
+  return(
+    <div className = "section__title--wrapper">
+      <h2 className = "section__title">{ title }</h2>
+      { sectionSubtitle }
+    </div>
+  );
+
+};
 
 export default SectionTitle;
