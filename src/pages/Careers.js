b0aa6f46bda382754578b487166c33266928e469
Iris Ibekwe <iris.ibekwe@civicactions.com>
2018-08-09 15:34:01 -0400

    CA-260: Refactor the image band component and reuse it in the Team page.

--- src/pages/Careers.js
+++ src/pages/Careers.js
@@ -6,9 +6,23 @@ import GeneralLayout from "./../components/layouts/GeneralLayout"
 import Benefits from '../components/organisms/Benefits';
 import ImageBand from './../components/organisms/ImageBand'
 
+// Image band Images
+import image1 from './../files/image-band/IMG_20170919_085448.jpg'
+import image2 from './../files/image-band/IMG_20170918_123826.jpg';
+import image3 from './../files/image-band/IMG_20170920_191202.jpg';
+import image4 from './../files/image-band/sacramento.jpg';
+import image5 from './../files/image-band/rachel-presentation.jpg';
+import image6 from './../files/image-band/20170919_174032_HDR.jpg';
+import image7 from './../files/image-band/IMG_20170918_182447.jpg';
+import image8 from './../files/image-band/IMG_20180713_130136.jpg';
+import image9 from './../files/image-band/IMG_0577.jpg';
+import image10 from './../files/image-band/IMG_20170918_122218.jpg';
+
 const Careers = ({data}) => {
   const{ allJob } = data;
   const { edges } = allJob;
+  const imageArray = [ image1, image2, image3, image4, image5, image6, image7, image8, image9, image10 ];
+
 
   const jobs = _.map(edges, (job, index) => {
 
@@ -36,7 +50,7 @@ const Careers = ({data}) => {
 
      <Benefits/>
       <section className = "feed__image--wrapper">
-        <ImageBand />
+        <ImageBand imageArray = { imageArray }/>
       </section>
 
     </GeneralLayout>
