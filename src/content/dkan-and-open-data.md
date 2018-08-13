f36dffdb210a2b01df0792f716413cc132ad460b
Alex <alex@alexscott.net>
2018-08-13 17:10:53 +0200

    CA-253: DKAN and open data page.

--- src/content/dkan-and-open-data.md
+++
@@ -1,7 +0,0 @@
----
-path: /
-type: page
-title: "Dkan and Open Data"
-subtitle: "We use DKAN to help organizations all over the world comply with open data mandates and harness the power of their data to demonstrate accomplishments, empower citizens to improve their communities, and educate the public." 
-images: []
----
---
+++ src/pages/Dkan-Open-Data.js
@@ -0,0 +1,73 @@
+ import React from 'react'
+import { graphql } from "gatsby"
+
+import IconParagraphsGroup from '../components/organisms/IconParagraphsGroup'
+import GeneralLayout from '../components/layouts/GeneralLayout';
+import SectionTitle from '../components/atoms/SectionTitle';
+import Button from "../components/atoms/Buttons";
+
+const Dkan = ({data}) => {
+  const{ markdownRemark } = data;
+  const{ frontmatter, html } = markdownRemark;
+  const{
+    features,
+    features_title,
+    title,
+    subtitle
+  } = frontmatter;
+
+  
+ return(
+   <GeneralLayout
+     heroTitle = { title }
+     heroSubtitle = { subtitle }
+     >
+     <section className = "section">
+       <div className = "usa-grid">
+         <p className = "text-container" dangerouslySetInnerHTML = {{ __html: html }} />
+
+         <div class = "align-center">
+           <Button
+             button_text = "Visit getDKAN.org"
+             link = "http://getdkan.org"
+             />
+         </div>
+
+       </div>
+     </section>
+
+     <section className = "section section_features usa-grid">
+       <SectionTitle title = { features_title }/>
+     <div className = "section__features--intro-text">DKAN comes out-of-the-box with comprehensive public-facing engagement and administrative site management tools.</div>
+       <IconParagraphsGroup icons = { features } />
+
+     </section>
+    </GeneralLayout>
+
+
+ );
+
+};
+
+export default Dkan;
+
+export const dkanOpenDataQuery = graphql`
+  query dkan {
+    markdownRemark(frontmatter: { title: { eq: "Dkan and Open Data" } } ) {
+    html
+    frontmatter {
+      title
+      subtitle
+      features_title
+      features {
+        title
+        caption
+        alt
+        icon {
+          publicURL
+        }
+      }
+    }
+  }
+  }
+`;
