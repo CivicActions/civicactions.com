5ac6a256ae7a201a0eec0fe4cbbaa6afa8fb0a3c
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-08-13 14:17:07 -0600

    CA-278: Only add external link class to external links.

--- src/components/scripts/ExternalLink.js
+++ src/components/scripts/ExternalLink.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 
 class ExternalLink extends Component {
     static addExternalClass() {
-        let anchors = document.querySelectorAll('a:not(.fa)');
+        let anchors = document.querySelectorAll('a:not(.fa):not([href^="/"])');
 
         for (let i = 0; i < anchors.length; i++) {
             if (anchors[i].host !== window.location.hostname) {
