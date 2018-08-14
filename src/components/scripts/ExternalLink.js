cb5193450c1e594db30820f061983e8d7fc77cdf
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-08-14 13:49:12 -0600

    CA-000: Check if document exists and is ready before apply external link js

--- src/components/scripts/ExternalLink.js
+++ src/components/scripts/ExternalLink.js
@@ -2,13 +2,20 @@ import React, { Component } from "react"
 
 class ExternalLink extends Component {
     static addExternalClass() {
-        let anchors = document.querySelectorAll('a:not(.fa):not([href^="/"])');
+        if(document) {
+            document.onreadystatechange = () => {
+                if (document.readyState === 'complete') {
+                    // document ready
+                    let anchors = document.querySelectorAll('a:not(.fa):not([href^="/"])');
 
-        for (let i = 0; i < anchors.length; i++) {
-            if (anchors[i].host !== window.location.hostname) {
-                anchors[i].setAttribute('class', 'external-link');
-            }
+                    for (let i = 0; i < anchors.length; i++) {
+                        if (anchors[i].host !== window.location.hostname) {
+                            anchors[i].setAttribute('class', 'external-link');
+                        }
 
+                    }
+                }
+            };
         }
 
         return null;
