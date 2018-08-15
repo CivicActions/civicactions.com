1f0b5cd2b5b30424311d2cd7a8a6ba7da3f0fa0f
Iris Ibekwe <iris.ibekwe@civicactions.com>
2018-08-15 11:15:04 -0400

    CA-269: Creating the ReachUs component.

---
+++ data/social-icons.js
@@ -0,0 +1,29 @@
+import icon1 from './../src/files/icons/contact/email.svg';
+import icon2 from './../src/files/icons/contact/phone.svg';
+import icon3 from './../src/files/icons/contact/mail.svg';
+
+const social_block = {social_icons:[
+  {
+    image: icon1,
+    title: "Email",
+    text: "contact@civicactions.com",
+    text2: "",
+    text3: ""
+  },
+  {
+    image: icon2,
+    title: "Phone",
+    text: "(510) 408-7510",
+    text2: "",
+    text3: ""
+  },
+  {
+    image: icon3,
+    title: "Mail",
+    text: "2625 Alcatraz Ave. ",
+    text2: "Suite 320",
+    text3: "Berkeley, CA 94705"
+  }
+] };
+
+export default social_block;
