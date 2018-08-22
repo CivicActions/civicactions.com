ed1a00acebd8ad2e648b84eeecafce908f4ca3db
Adrienne Cabouet <adrienne.cabouet@gmail.com>
2018-08-22 12:34:09 -0600

    CA-364: Add extra dkan button to /dkan

--- src/pages/dkan.js
+++ src/pages/dkan.js
@@ -43,6 +43,17 @@ const Dkan = ({data}) => {
          <IconParagraphsGroup icons = { features } />
        </section>
      </section>
+
+       <section className="section section_dkan_cta">
+           <div className="usa-grid">
+               <div className="align-center">
+                   <Button
+                       button_text="Visit getDKAN.org"
+                       link="http://getdkan.org"
+                   />
+               </div>
+           </div>
+       </section>
     </GeneralLayout>
 
 
