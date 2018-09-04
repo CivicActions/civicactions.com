2e573f60b02e69e64389b038e471a0e9b66bb473
Alex <alex@alexscott.net>
2018-09-04 20:02:08 +0200

    CA-390: Adding jest testing infrastructure.

---
+++ src/__tests__/t1-test.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+//import renderer from "react-test-renderer"
+
+describe("Test test.", () => {
+  it('Should have tests', () => {
+    expect(1).toEqual(1);
+  });
+});
+
+
