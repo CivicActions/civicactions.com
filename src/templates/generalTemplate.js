f97329f28cd6d444155774a5c6c46bbe980c029e
Alex <alex@alexscott.net>
2018-12-05 17:01:28 +0100

    CA-310: no need for the extra template file, and adding section for padding at page bottom.

--- src/templates/generalTemplate.js
+++ src/templates/generalTemplate.js
@@ -15,7 +15,7 @@ export default function Template({data}) {
         heroSubtitle = { frontmatter.subtitle }
         pageTitle = { `CivicActions | ${frontmatter.title}` }
       >
-        <div className = "text-container">
+        <div className = "text-container section">
           <div dangerouslySetInnerHTML = {{ __html: html}} />
         </div>
     </GeneralLayout>
