39d7339c18eb7fdc8c87315f844767882bae3f98
Alex <alex@alexscott.net>
2019-01-10 18:24:54 +0100

    CA-434: adding hero link for pdf as plain href link.

--- src/components/atoms/Buttons.js
+++ src/components/atoms/Buttons.js
@@ -4,16 +4,21 @@ import Link from "./../../components/scripts/Link";
 
 
 const Button = ({type, button_text, link, isExternal}) => {
+  let buttonURL = null;
+
   const buttonClass =
     (type === 'secondary') ? 'usa-button-secondary':
     (type === 'dark') ? 'usa-button-secondary-inverse':
     (type === 'hero') ? 'usa-button-navy':
       'usa-button';
 
-  const buttonURL =  <Link
-      to = { link }
-      children = { button_text }
-  />
+  if (type === 'hero' && (link.charAt(0) === '/' && link.slice(-4) === '.pdf')) {
+    buttonURL = <a href={link}>{button_text}</a>
+  } else {
+    buttonURL =  <Link
+    to = { link }
+    children = { button_text } />
+  };
 
   return (
     <button className = { buttonClass }>
