0bfa4db83b7f3c11907e0eb842f8de913352bd72
Alex Scott <alexiscott@users.noreply.github.com>
2019-07-18 10:28:40 +0200

    CA-379: tests for Gatsby Jazz URL value detection.

--- src/__tests__/jazz-fake-content-test.js
+++ src/__tests__/jazz-fake-content-test.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-describe('Gatsby fake content', () => {
-  it('Should know which type of develpment environment we are using.', () => {
-    expect(process.env.NODE_ENV
-    ).toEqual('test');
+describe('JAZZ API URL', () => {
+  it('Should use the dummy page when "development"', () => {
+    const GATSBY_JAZZ_URL = 'development';
+    expect(GATSBY_JAZZ_URL).toEqual('development');
   });
+  it('Should pass when GATSBY_JAZZ_URL is not empty',
+     () => {
+       const GATSBY_JAZZ_URL = '';
+       expect(GATSBY_JAZZ_URL === '' ).toBeTruthy();
+     })
+  it('Should pass when GATSBY_JAZZ_URL is null',
+     () => {
+       const GATSBY_JAZZ_URL = null;
+       expect(GATSBY_JAZZ_URL === null).toBeTruthy();
+     })
+    it('Should pass when GATSBY_JAZZ_URL is any string other than development.',
+     () => {
+       const GATSBY_JAZZ_URL = 'http://somerandomstring';
+       expect(GATSBY_JAZZ_URL).not.toBe('development');
+     })
 });
+
+
