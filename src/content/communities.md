e1fe7cb50630111f97b3b690e4e5d03b5d8b8ce0
Alex Scott <alex@alexscott.net>
2019-09-17 16:54:35 +0200

    Redesign the communities page.

--- src/content/communities.md
+++ src/content/communities.md
@@ -3,13 +3,25 @@ path: /
 type: page
 title: Our Communities
 subtitle: We believe in the power of community to help transform government and the world. When people come together to share ideas and combine their talents, great things happen.
-agile_intro_text: CivicActions founded and supports Agile Government Leadership, a nonprofit network of government professionals working to bring agile and innovative practices to the public sector.
-dkan_title: DKAN Open Data Platform
-dkan_intro-text: CivicActions maintains DKAN, a community-driven, free and open source open data platform used by governments, organizations, and individuals around the world to leverage data for the public good.
-agl_logo: "general-images/agl-2x.png"
-agl_button_text: Visit AGL
-agl_button_link: https://www.agilegovleaders.org
-dkan_logo: "general-images/dkan2x.png"
-dkan_button_text: Visit DKAN
-dkan_button_link: "http://getdkan.org"
 ---
+<h3 class="communities-with-agl-logo">Agile Government Leadership</h3>
+<p>
+CivicActions founded and supports Agile Government Leadership, a nonprofit network of government professionals working to bring agile and innovative practices to the public sector. 	
+</p>
+
+[Visit AGL](https://www.agilegovleaders.org/)
+
+
+<h3 class="with-dkan-logo">DKAN Open Data Platform</h3>
+<p>
+CivicActions maintains DKAN, a community-driven, free and open source open data platform used by governments, organizations, and individuals around the world to leverage data for the public good.
+</p>
+
+[Visit DKAN](http://getdkan.org)
+
+
+<h3 class="with-digital-services-coalition">Digital Services Coalition</h3>
+<p>CivicActions is a founding member of the Digital Services Coalition, a group of agile design and technology firms collaboratiing with each other to help government build better services.
+</p>
+
+[Visit DSC](https://digitalservicescoalition.org)
--- src/pages/communities.js
+++ src/pages/communities.js
@@ -9,63 +9,21 @@ import GeneralLayout from "./../components/layouts/GeneralLayout"
 
 const Communities = ({ data }) => {
   const { markdownRemark } = data
-  const { frontmatter } = markdownRemark
-  const {
-    agl_button_text,
-    agl_button_link,
-    title,
-    subtitle,
-    agile_intro_text,
-    dkan_title,
-    dkan_button_text,
-    dkan_button_link,
-    dkan_intro_text,
-    agl_logo,
-    dkan_logo,
-  } = frontmatter
-
-  const aglLogo = agl_logo ? agl_logo.childImageSharp.resolutions : ``
-  const dkanLogo = dkan_logo ? dkan_logo.childImageSharp.resolutions : ``
+  const { frontmatter, html } = markdownRemark
 
   return (
-    <GeneralLayout heroTitle={title} heroSubtitle={subtitle}>
-      {/* The Recent AGL Posts from Medium Section.*/}
-      <section className="section section__recent-posts">
-        <div className="usa-grid community__wrapper">
-          <span className="community__logo">
-            <Img resolutions={aglLogo} />
-          </span>
-          <SectionTitle
-            title="Agile Government Leadership"
-            subtitle={agile_intro_text}
-          >
-            <Link
-              to={agl_button_link}
-              children={agl_button_text}
-              className="link-button external-link"
-            />
-          </SectionTitle>
-          AGL POSTS
-        </div>
-      </section>
+    <GeneralLayout heroTitle={frontmatter.title} heroSubtitle={frontmatter.subtitle}>
 
-      {/* The Recent DKAN Posts from Medium Section.*/}
-      <section className="section section__recent-posts right-flipped">
-        <div className="usa-grid community__wrapper">
-          <span className="community__logo dkan">
-            <Img resolutions={dkanLogo} />
-          </span>
-          <SectionTitle title={dkan_title} subtitle={dkan_intro_text}>
-            {/* Passing in a link component as props.children.  */}
-            <Link
-              to={dkan_button_link}
-              children={dkan_button_text}
-              className="link-button external-link"
-            />
-          </SectionTitle>
-          DKAN POSTS
-        </div>
+      <section className="section section__communities">
+      <div className="usa-grid">
+      <div
+    className="text-container"
+    dangerouslySetInnerHTML={{ __html: html }}
+      />
+      </div>
       </section>
+    
+      
     </GeneralLayout>
   )
 }
@@ -75,30 +33,10 @@ export default Communities
 export const communitiesQuery = graphql`
   query communityPosts {
     markdownRemark(frontmatter: { title: { eq: "Our Communities" } }) {
-      frontmatter {
-        agl_button_text
-        agl_button_link
+      html
+      frontmatter {      
         title
         subtitle
-        agile_intro_text
-        dkan_button_text
-        dkan_button_link
-        dkan_intro_text
-        dkan_title
-        agl_logo {
-          childImageSharp {
-            resolutions(width: 67, height: 67) {
-              ...GatsbyImageSharpResolutions_withWebp
-            }
-          }
-        }
-        dkan_logo {
-          childImageSharp {
-            resolutions(width: 67, height: 67) {
-              ...GatsbyImageSharpResolutions_withWebp
-            }
-          }
-        }
       }
     }
   }
