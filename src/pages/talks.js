add0b8babca0478fd8591139e13ebce181bc36d8
Alex Scott <alex@alexscott.net>
2019-11-29 17:21:16 +0100

    CA-499: "What we've been talking about", on the talks page.

--- src/pages/talks.js
+++ src/pages/talks.js
@@ -1,8 +1,12 @@
 import React from "react"
 import { graphql } from "gatsby"
+import _ from "lodash"
+
 import YouTubeVideo from "./../components/atoms/YouTubeVideo"
 import GeneralLayout from "./../components/layouts/GeneralLayout"
 import SectionTitle from "../components/atoms/SectionTitle"
+import Teaser from "./../components/Teaser"
+import Link from "./../components/scripts/Link.js"
 
 const Talks = ({ data }) => {
   const { markdownRemark } = data
@@ -14,11 +18,29 @@ const Talks = ({ data }) => {
     videos_title,
     related_link_title,
     related_link,
+    what_we_have_been_talking_about_teasers,
   } = frontmatter
 
   const videoTeasers = videos.map((video, index) => {
     return <YouTubeVideo key={index} video={video} />
   })
+
+  const whatWeHaveBeenTalkingAboutTeasers = _.map(
+    what_we_have_been_talking_about_teasers,
+    (item, index) => {
+      const { title, link, date } = item
+      return (
+        <li className="what-weve-been-talking-about--teaser__item teaser__item">
+          <Teaser
+            key={{ index }}
+            teaserDate={date}
+            teaserTitle={title}
+            teaserLink={link}
+          />
+        </li>
+      )
+    }
+  )
 
   return (
     <GeneralLayout heroTitle={title} heroSubtitle={subtitle}>
@@ -28,14 +50,31 @@ const Talks = ({ data }) => {
       <section className="section usa-grid section__talks">
         {videoTeasers}
       </section>
+
+      {/* What we've been talking about */}
+      <section className="section section__what-weve-been-talking-about">
+        <div className="usa-grid">
+          <SectionTitle title="What we've been talking about" />
+          <div className="teaser-wrapper">
+            <ul className="what-weve-been-talking-about--teasers">
+              {whatWeHaveBeenTalkingAboutTeasers}
+            </ul>
+            <Link
+              children="See more talks"
+              to="https://medium.com/civicactions/tagged/civicactions-talks"
+              className="link-button"
+            />
+          </div>
+        </div>
+      </section>
     </GeneralLayout>
   )
 }
 
 export default Talks
 
-export const talksQuery = graphql`
-  query talksPage {
+export const query = graphql`
+  query talksContent {
     markdownRemark(frontmatter: { title: { eq: "Talks" } }) {
       html
       frontmatter {
@@ -46,6 +85,11 @@ export const talksQuery = graphql`
           url
           related_link_title
           related_link
+        }
+        what_we_have_been_talking_about_teasers {
+          date
+          title
+          link
         }
       }
     }
