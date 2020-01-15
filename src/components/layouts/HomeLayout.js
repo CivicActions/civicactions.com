cd29e552d66b3f748cc4c6d392bd896e5d1ed0c9
Cameron Prince <cameron@teslauniverse.com>
2020-01-15 16:44:40 -0600

    CA-463: Updates home page og:image image.

--- src/components/layouts/HomeLayout.js
+++ src/components/layouts/HomeLayout.js
@@ -11,6 +11,7 @@ import Footer from "./../footer/Footer"
 import config from "../../../data/SiteConfig"
 import TopNav from "./../navigation/TopNav"
 import header_bg from "./../header/background_bg-hero.png"
+import logo from "./../header/logo.png"
 
 const Layout = ({ children, data, location }) => (
   <StaticQuery
@@ -71,7 +72,7 @@ const Layout = ({ children, data, location }) => (
               property: `og:description`,
               content: `Open and Agile Digital Government Services`,
             },
-            { property: `og:image`, content: header_bg },
+            { property: `og:image`, content: logo },
             { name: `twitter:card`, content: config.seo.twitterCard },
             { name: `twitter:site`, content: config.seo.twitterSite },
             { property: `fb:app_id`, content: config.seo.fbAppId },
