c6e7f77382000defaf30a600c932696cccfbe81d
Brian Seek <brian.seek@gmail.com>
2020-01-15 16:09:45 -0800

    CA-493: add Person based 404 page.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,17 @@
 import React from "react"
+import PropTypes from "prop-types"
 import NotFoundLayout from "../components/layouts/NotFoundLayout"
+import PersonNotFoundLayout from "../components/layouts/PersonNotFoundLayout"
 
-const NotFoundPage = () => <NotFoundLayout></NotFoundLayout>
+class NotFoundPage extends React.Component {
+  static propTypes = {
+    location: PropTypes.object,
+  }
+
+  render() {
+    const {pathname} = this.props.location
+    return pathname.includes('/team/') ? <PersonNotFoundLayout/> : <NotFoundLayout/>
+  }
+}
 
 export default NotFoundPage
