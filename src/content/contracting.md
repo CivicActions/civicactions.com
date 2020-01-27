a149aa297e751f73c53dd00e869331818b2ce5cd
Cameron Prince <cameron@teslauniverse.com>
2020-01-27 10:21:32 -0600

    CA-503: Adds PDF linking.

--- src/content/contracting.md
+++ src/content/contracting.md
@@ -7,6 +7,3 @@ images: []
 ---
 
 ### Contracting Vehicles
-
-* CMAS Schedule 3-16-70-3298A (PDF)
-* GSA Schedule 70 (PDF)
--- src/pages/contracting.js
+++ src/pages/contracting.js
@@ -3,6 +3,10 @@ import { graphql } from "gatsby"
 
 import GeneralLayout from "./../components/layouts/GeneralLayout"
 
+import gsaSchedule from "./../content/docs/GSA-Schedule.pdf"
+import cmasSchedule from "./../content/docs/CMAS-Schedule.pdf"
+import Link from "./../components/scripts/Link"
+
 class Contracting extends React.Component {
   render() {
     const { markdownRemark } = this.props.data
@@ -15,14 +19,34 @@ class Contracting extends React.Component {
         heroSubtitle={subtitle}
         urlObject={this.props.location}
       >
+
         <section className="section">
           <div className="usa-grid">
             <div
               className="text-container"
               dangerouslySetInnerHTML={{ __html: html }}
             />
+            <div class="text-container">
+            <ul>
+              <li>
+                <Link
+                  to={gsaSchedule}
+                  children="GSA Schedule 70 (PDF)"
+                  className=""
+                />
+              </li>
+              <li>
+                <Link
+                  to={cmasSchedule}
+                  children="CMAS Schedule 3-16-70-3298A (PDF)"
+                  className=""
+                />
+              </li>
+            </ul>
+            </div>
           </div>
         </section>
+
       </GeneralLayout>
     )
   }
