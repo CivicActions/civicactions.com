4dc56729a9450355dd7f24d99b151486a1c0c068
Cameron Prince <cameron@teslauniverse.com>
2020-03-12 13:01:35 -0500

    CA-525: Adjusts hosts value.

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: "https://civicactions.com",
+        host: "civicactions.com",
         sitemap: null,
         policy: [{ userAgent: "*", allow: "/" }],
       },
