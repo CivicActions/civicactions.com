55f6c08f43108270648535b6d6b1621344bc5a4a
Yodart <joaogabrielgcoutinho@gmail.com>
2021-02-15 19:36:20 -0300

    Removed Unused Commented Code

--- src/templates/press.js
+++ src/templates/press.js
@@ -4,8 +4,17 @@ import Link from "gatsby-link"
 import GeneralLayout from "./../components/layouts/GeneralLayout"
 import PressTeaser from "./../components/PressTeaser"
 
+// TODO: Implement previous and next links to paginate the press pages
+
 const Press = (props) => {
   const data = useStaticQuery(query);
+  // const NavLink = props => {
+  //   if (!props.test) {
+  //     return <Link to={props.url}>{props.text}</Link>
+  //   } else {
+  //     return <span>{props.text}</span>
+  //   }
+  // }
   return (
     <GeneralLayout
       heroTitle="Press"
@@ -28,6 +37,14 @@ const Press = (props) => {
           </div>
         })} 
         </ul>
+        {/* <div className="prev-next-links">
+          <div className="previous">
+            <NavLink test={first} url={previousUrl} text="Previous" />
+          </div>
+          <div className="next">
+            <NavLink test={last} url={nextUrl} text="Next" />
+          </div>
+        </div> */}
         </div>
       </section>
     </GeneralLayout>
@@ -51,78 +68,4 @@ export const query = graphql`
     }
   }
 `
-
-
-// OLD CODE TO BE USED AS REFERENCE WHILE BUILDING CURRENT
-
-// const NavLink = props => {
-//   if (!props.test) {
-//     return <Link to={props.url}>{props.text}</Link>
-//   } else {
-//     return <span>{props.text}</span>
-//   }
-// }
-
-// const Press = ({ data, pageContext, location }) => {
-//   // pagination. See gatsby node for loading of pageContext.
-//   const { group, index, first, last } = pageContext
-//   const previousUrl =
-//     index - 1 === 1 ? `/press` : `/press/` + (index - 1).toString()
-//   const nextUrl = `/press/` + (index + 1).toString()
-//   const { markdownRemark } = data
-//   const { frontmatter } = markdownRemark
-//   const { title, subtitle } = frontmatter
-
-//   const pressItems = group.map(({ node }, index) => {
-//     const { frontmatter, html } = node
-//     const { title, date, publication, link_text, website } = frontmatter
-
-//     return (
-//       <PressTeaser
-//         publication={publication}
-//         date={date}
-//         key={index}
-//         title={title}
-//         text={html}
-//         link_text={link_text}
-//         website={website}
-//       />
-//     )
-//   })
-
-//   return (
-//     <GeneralLayout
-//       pageTitle={`CivicActions | ${title}`}
-//       heroTitle={title}
-//       heroSubtitle={subtitle}
-//       urlObject={location}
-//     >
-//       <section className="section">
-//         <div className="usa-grid text-container">
-//           <ul className="press__list">{pressItems}</ul>
-//           <div className="prev-next-links">
-//             <div className="previous">
-//               <NavLink test={first} url={previousUrl} text="Previous" />
-//             </div>
-//             <div className="next">
-//               <NavLink test={last} url={nextUrl} text="Next" />
-//             </div>
-//           </div>
-//         </div>
-//       </section>
-//     </GeneralLayout>
-//   )
-// }
-// export default Press
-
-// export const pressQuery = graphql`
-//   query pressPage {
-//     markdownRemark(frontmatter: { title: { eq: "Press" } }) {
-//       frontmatter {
-//         title
-//         subtitle
-//       }
-//     }
-//   }
-// `
 
